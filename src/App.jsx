d01b05f282e2978bbea0328f18ffccd8e0ffcd16
itsnilayyy@gmail.com <ayenilay@Nilays-MacBook-Air.local>
2026-06-18 18:11:21 +0530

    Added change to check hosting status

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <>
       <h1>Learn react from chaicode.com</h1>
-      <p>This is a paragraph</p>
+      <p>This is a change that i have made.</p>
     </>
   )
 }
